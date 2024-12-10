b53700eabd7f883dbeec31267ecf3aef0caddb5d
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-10 18:00:22 -0300

    feat : priemeira atividade

--- ExameChunin/Program.cs
+++ ExameChunin/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+System.Console.WriteLine("Insira P1 (A, B ou N)");
+string p1 =  Console.ReadLine();
+
+
+System.Console.WriteLine("Insira P2 (A, B ou N)");
+string p2 =  Console.ReadLine();
+
+p1 = p1.ToUpper();
+p2 = p2.ToUpper();
+
+if(p1.Equals("N") || p2.Equals("N")){
+    System.Console.WriteLine("eliminado");
+}
+
+else if (p1.Equals(p2)){
+    System.Console.WriteLine("eliminado");
+}
+
+else{
+    System.Console.WriteLine("classificado");
+}
+
+
