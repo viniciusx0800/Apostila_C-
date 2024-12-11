fcabbdec647426fbe25daab99eabeb6c65e06bea
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-11 17:19:52 -0300

    feat : ajustes

--- 02Continha/Program.cs
+++ 02Continha/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
-
+System.Console.WriteLine("");
+System.Console.WriteLine("A entrada contém 6 valores inteiros:");
+System.Console.WriteLine("");
+System.Console.WriteLine("<<<<<<<<<< CONTINHAS >>>>>>>>>>");
+System.Console.WriteLine("");
 static double calc (double a, double b , double c, double d, double e, double f)
 {
     var calculo = ((a+b)*(c-d)*(e+f))/2 ;
@@ -10,6 +14,10 @@ var soma1 = calc (7,3,15,30.4,0,2);
 var soma2 = calc (1,2,10,5.0,2,2);
 
 
-System.Console.WriteLine("Sou fera nas continhas e o resultado é " + soma1);
-System.Console.WriteLine("Sou fera nas continhas e o resultado é " + soma2);
+System.Console.WriteLine("Sou FERA nas continhas e o resultado é " + soma1);
+System.Console.WriteLine("");
+System.Console.WriteLine("Sou FERA nas continhas e o resultado é " + soma2);
 
+System.Console.WriteLine("");
+System.Console.WriteLine("<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>");
+System.Console.WriteLine("Fim Algoritimo");
