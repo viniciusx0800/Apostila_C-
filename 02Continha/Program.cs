f1062742af4a38f608807a6577c66bcf4ca36295
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-11 16:33:01 -0300

    feat: atividade continha

---
+++ 02Continha/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+static double calc (double a, double b , double c, double d, double e, double f)
+{
+    var calculo = ((a+b)*(c-d)*(e+f))/2 ;
+
+    return calculo; 
+}
+var soma1 = calc (7,3,15,30.4,0,2);
+var soma2 = calc (1,2,10,5.0,2,2);
+
+
+System.Console.WriteLine("Sou fera nas continhas e o resultado é " + soma1);
+System.Console.WriteLine("Sou fera nas continhas e o resultado é " + soma2);
+
--- ExameChunin/Program.cs
+++ ExameChunin/Program.cs
@@ -1,26 +1,33 @@
-﻿
-
-
-System.Console.WriteLine("Insira P1 (A, B ou N)");
+﻿using System;
+System.Console.WriteLine("");
+System.Console.WriteLine("A entrada é composta por  dois pergaminho “P1” e “P2”, e");
+System.Console.WriteLine("representando a característica de cada pergaminho. Portanto");
+System.Console.WriteLine(" “P1” e “P2” podem ser “A” ou “B” e “N” sem pergaminho");
+System.Console.WriteLine("");
+System.Console.WriteLine( "<<<<<<<<<< EXAME CHUNIN >>>>>>>>>>");
+System.Console.WriteLine("");
+Console.WriteLine("Insira P1 (A, B ou N)");
 string p1 =  Console.ReadLine();
 
-
-System.Console.WriteLine("Insira P2 (A, B ou N)");
+System.Console.WriteLine("");
+Console.WriteLine("Insira P2 (A, B ou N)");
 string p2 =  Console.ReadLine();
 
 p1 = p1.ToUpper();
 p2 = p2.ToUpper();
 
 if(p1.Equals("N") || p2.Equals("N")){
-    System.Console.WriteLine("eliminado");
+    Console.WriteLine("eliminado");
 }
 
 else if (p1.Equals(p2)){
-    System.Console.WriteLine("eliminado");
+    Console.WriteLine("eliminado");
 }
 
 else{
-    System.Console.WriteLine("classificado");
+    Console.WriteLine("classificado");
 }
 
-
+System.Console.WriteLine("");
+System.Console.WriteLine("<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>");
+System.Console.WriteLine("Fim Algoritimo");
