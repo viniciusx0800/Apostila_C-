98e18899fbf429f72e84b2e2b3f32a7745f3d9dc
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-12 09:12:34 -0300

    feat : atividade contador segundos

---
+++ 07ContadorDeSegundos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Console.WriteLine("");
+Console.WriteLine("Será dado um número inteiro N que representa o tempo do evento em segundos.");
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<< CONTADOR DE SEGUNDOS >>>>>>>>>>");
+Console.WriteLine("");
+Console.WriteLine("Digite um número");
+
+int n = int.Parse(Console.ReadLine());
+
+int horas = n /3600;
+
+int minutos = (n % 3600) / 60;
+
+int segundos = n % 60;
+
+Console.WriteLine("");
+Console.WriteLine("Contém o tempo dado em segundos convertido para horas, minutos e segundos");
+Console.WriteLine("");
+Console.WriteLine($"{horas}h {minutos}m {segundos}s");
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<<(^.^)>>>>>>>>>>>>>>>>");
+Console.WriteLine("Fim Algoritimo");    
