b1c8c48c54ce59c9a88e1a0f7bf809c154ccbd77
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-13 13:57:54 -0300

    feat : ajustes no codigo

--- 13BolinhaDeGude/Program.cs
+++ 13BolinhaDeGude/Program.cs
@@ -1,9 +1,27 @@
 ﻿
-Console.WriteLine("quantidade de famia");
-int famia = int.Parse(Console.ReadLine());
-Console.WriteLine("quantidade de gude que a  famia deu");
+Console.WriteLine("");
+Console.WriteLine("A entrada é composta por um número inteiro N , que representa a"); 
+Console.WriteLine("quantidade de familiares que irão presenteá-lo com bolinhas de gude e um");
+Console.WriteLine("inteiro Q , que representa a quantidade de bolinhas que o primeiro");
+Console.WriteLine("familiar dará de presente");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<< BOLINHAS DE GUDE >>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("");
+
+
+Console.WriteLine("Digite quantidade de familiares");
+int familha = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite quantas gudes que o primeiro familiar deu");
 int gude = int.Parse(Console.ReadLine());
 
-int totalBolinhas = gude * ((int)Math.Pow(2, famia) - 1);
+int totalBolinhas = gude * ((int)Math.Pow(2, familha) - 1);
 
-Console.WriteLine(totalBolinhas);
+Console.WriteLine("");
+
+Console.WriteLine($"Yuri ganhou {totalBolinhas} bolinhas de gude");
+
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<(^.^)>>>>>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("Fim Algoritimo");   
+		
