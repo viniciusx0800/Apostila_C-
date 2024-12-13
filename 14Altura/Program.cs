0e1e2bd7620909477b05b7a1aebcf23cba7d9a37
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-13 14:40:34 -0300

    feat : Atividade da Altura

---
+++ 14Altura/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("");
+Console.WriteLine("A entrada  ́e composta por três numeros inteiros diferentes,");
+Console.WriteLine("a altura de cada um dos amigos  respectivamente, separados por um espaco em branco");
+// Console.WriteLine("================== \(¨-¨)/ ===================");
+System.Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<< ALTURA >>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("");
+
+int maior = 0;
+
+Console.WriteLine("Digite quantos amigos são");
+int quantidade = int.Parse(Console.ReadLine());
+
+for(int i = 0; quantidade > i; i++)
+{
+    System.Console.WriteLine("Digite a altura de cada um dos amigos (¨-¨) ");
+    int altura = int.Parse(Console.ReadLine());
+    if(altura > maior)
+    {
+        maior = altura;
+    }    
+}
+
+Console.WriteLine("");
+Console.WriteLine($"O maior tem a altura de  : {maior} m");
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<<(^.^)>>>>>>>>>>>>>>>>");
+Console.WriteLine("Fim Algoritimo");    
