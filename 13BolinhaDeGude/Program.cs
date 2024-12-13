6e685d556b29591f61dc6090857bfc4253d1bb7c
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-13 09:21:33 -0300

    feat : atividade Bolinha de gude

---
+++ 13BolinhaDeGude/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("quantidade de famia");
+int famia = int.Parse(Console.ReadLine());
+Console.WriteLine("quantidade de gude que a  famia deu");
+int gude = int.Parse(Console.ReadLine());
+
+int totalBolinhas = gude * ((int)Math.Pow(2, famia) - 1);
+
+Console.WriteLine(totalBolinhas);
