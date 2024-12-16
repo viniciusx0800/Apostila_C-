ff321b861c9e0ce35217f632c9ec95c9a9880bc0
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-16 09:48:43 -0300

    feat : Atividade Forjando espada

---
+++ 19ForjandoEspadas/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<<<< FORJANDO ESPADAS >>>>>>>>>la ele>>>>>>>>>>");
+Console.WriteLine("");
+
+
+Console.WriteLine("Digite a Quantidade de aço");
+int QuantidadeAco = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a Quantidade de madeira");
+int QuantidadeMadeira = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a Quantidade de couro");
+int QuantidadeCouro = int.Parse(Console.ReadLine());
+
+
+int aco = QuantidadeAco / 2;
+
+int madeira = QuantidadeMadeira / 3;
+
+int couro = QuantidadeCouro / 5;
+
+
+int espadas = Math.Min(aco, Math.Min(madeira, couro));
+
+Console.WriteLine("");
+Console.WriteLine($"sera possivel fazer : {espadas} ESPADAS");
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<(^.^)>>>>>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("Fim do Algoritimo");    
