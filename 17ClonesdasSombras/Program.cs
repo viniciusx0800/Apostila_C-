cd45878d057d201d05f8c8e4ce2d2170d38edb0e
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-16 08:35:45 -0300

    feat : Atividade clones das sombras

---
+++ 17ClonesdasSombras/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.WriteLine("");
+Console.WriteLine("A primeira linha da entrada consiste de um inteiro 'N',");
+Console.WriteLine("que representa a quantidade de ninjas inimigos.");
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<< CLONES DAS SOMBRAS >>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("");
+
+Console.WriteLine("Digite a quantiade de ninjas");
+int ninjas = int.Parse(Console.ReadLine());
+
+if ((ninjas & ninjas - 1) == 0)
+{
+    Console.WriteLine("Dattebayo");
+}
+else{
+    Console.WriteLine("Tururuuu");
+}
+
+Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<(^.^)>>>>>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("Fim do Algoritimo");    
