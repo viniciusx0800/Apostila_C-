35610dae0034eda4cbcbb004cb2c939307b25f87
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-17 14:00:51 -0300

    feat : Ajustes na atividade do flash

--- 21EnergiaDeAceleração/Program.cs
+++ 21EnergiaDeAceleração/Program.cs
@@ -1,9 +1,20 @@
-﻿
-Console.WriteLine("Digite qual a massa do flash");
+﻿Console.WriteLine("");
+Console.WriteLine("A entrada consiste em dois valores inteiros positivos correspondentes à");
+Console.WriteLine("massa atual do Flash em quilos e a velocidade em que o");
+Console.WriteLine("Flash está correndo em determinado momento, em metros por segundo");
+Console.WriteLine("");
+Console.WriteLine(">>>>>>>>>>>>>>>>> MEU NOME É Barry Allen >>>>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("");
+
+Console.WriteLine("Digite a massa do flash");
 int massa = int.Parse(Console.ReadLine());
-Console.WriteLine("Digite qual a velocidade do flash");
+Console.WriteLine("Digite a velocidade do flash");
 int velocidade = int.Parse(Console.ReadLine());
 
 int energia = massa * velocidade * velocidade;
 
-Console.WriteLine($"ele presirara de : {energia} energia");
+Console.WriteLine("");
+Console.WriteLine($"A energia necessária para o Flash é de : {energia} ");
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<(^.^)>>>>>>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("Fim do Algoritimo");    
