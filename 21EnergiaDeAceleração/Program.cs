28d4069bce0167233e8c5fa46152094d86f9def9
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-17 09:08:51 -0300

    feat : Atividade do flash

---
+++ 21EnergiaDeAceleração/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Digite qual a massa do flash");
+int massa = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite qual a velocidade do flash");
+int velocidade = int.Parse(Console.ReadLine());
+
+int energia = massa * velocidade * velocidade;
+
+Console.WriteLine($"ele presirara de : {energia} energia");
