0acc9e02e6fd4f79e261d55335cb11e8c9b3545e
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-17 08:50:24 -0300

    feat : Atividade do Skywalker

---
+++ 20AjudeSkywalker/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("");
+Console.WriteLine("A entrada é composta por cinco inteiros, 'A', 'B', 'C','D' e 'E', representando");
+Console.WriteLine("respectivamente, o total de naves sondadas no quadrante, o total de naves amigas detectadas a");
+Console.WriteLine("frente de Skywalker, o total de naves amigas a direita, o total de naves amigas a esquerda e o");
+Console.WriteLine("total de naves amigas atrás da nave de Skywalker.");
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<<<< AJUDE SKYWALKER >>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("");
+
+Console.WriteLine("Digite o total de naves sondadas no quadrante 'A' ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o número de naves amigas à frente de Skywalker 'B' ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o número de naves amigas à direita 'C' ");
+int C = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o número de naves amigas à esquerda 'D' ");
+int D = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o número de naves amigas atrás de Skywalker 'E' ");
+int E = int.Parse(Console.ReadLine());
+
+
+int navesInimigas = A - (B + C + D + E);
+
+Console.WriteLine("");
+Console.WriteLine($"total de naves inimigas são de: {navesInimigas}");
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<(^.^)>>>>>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("Fim do Algoritimo");    
