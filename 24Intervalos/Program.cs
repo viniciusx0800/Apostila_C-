e630a0ea08f761c18e8fa7da97c95807fccccb95
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-18 15:26:27 -0300

    feat : Ajustar Atividade

--- 24Intervalos/Program.cs
+++ 24Intervalos/Program.cs
@@ -1,40 +1,51 @@
 ﻿using System;
 
-class Program
-{
-    static void Main()
+    Console.WriteLine("");
+    Console.WriteLine("A entrada contém três linhas: a primeira corresponde ao primeiro intervalo");
+    Console.WriteLine("(]x,y]); a segunda corresponde ao segundo intervalo ([w,z[); e a terceira");
+    Console.WriteLine("corresponde ao número inteiro que se quer descobrir o intervalo ao qual");
+    Console.WriteLine("pertence.");
+    Console.WriteLine("");
+    Console.WriteLine("<<<<<<<<<<<<<<<<<<<< INTERVALOS >>>>>>>>>>>>>>>>>>>");
+    Console.WriteLine("");
+    
+    Console.WriteLine("Digite os dois números do primeiros intervalo");
+    string[] primeiroIntervalo = Console.ReadLine().Split();
+    Console.WriteLine("Digite os dois números do Segundos intervalo");
+    string[] segundoIntervalo = Console.ReadLine().Split();
+    Console.WriteLine("Digite um numero para ver a qual intervalo pertence");
+    int numero = int.Parse(Console.ReadLine());
+
+    int x = int.Parse(primeiroIntervalo[0]);
+    int y = int.Parse(primeiroIntervalo[1]);
+    int w = int.Parse(segundoIntervalo[0]);
+    int z = int.Parse(segundoIntervalo[1]);
+
+    bool noPrimeiroIntervalo = (numero > x && numero <= y);
+    bool noSegundoIntervalo = (numero >= w && numero < z);
+
+    
+    if (noPrimeiroIntervalo && noSegundoIntervalo)
     {
-        Console.WriteLine("Digite os dois números do primeiros intervalo");
-        string[] primeiroIntervalo = Console.ReadLine().Split();
-        Console.WriteLine("Digite os dois números do Segundos intervalo");
-        string[] segundoIntervalo = Console.ReadLine().Split();
-        Console.WriteLine("Digite um numero para ver a qual intervalo pertence");
-        int numero = int.Parse(Console.ReadLine());
-
-        int x = int.Parse(primeiroIntervalo[0]);
-        int y = int.Parse(primeiroIntervalo[1]);
-        int w = int.Parse(segundoIntervalo[0]);
-        int z = int.Parse(segundoIntervalo[1]);
-
-        bool noPrimeiroIntervalo = (numero > x && numero <= y);
-        bool noSegundoIntervalo = (numero >= w && numero < z);
-
-      
-        if (noPrimeiroIntervalo && noSegundoIntervalo)
-        {
-            Console.WriteLine("Ambos!");
-        }
-        else if (noPrimeiroIntervalo)
-        {
-            Console.WriteLine("Primeiro intervalo!");
-        }
-        else if (noSegundoIntervalo)
-        {
-            Console.WriteLine("Segundo intervalo!");
-        }
-        else
-        {
-            Console.WriteLine("Nenhum!");
-        }
+        Console.WriteLine("");
+        Console.WriteLine("Ambos!");
     }
-}
+    else if (noPrimeiroIntervalo)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Primeiro intervalo!");
+    }
+    else if (noSegundoIntervalo)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Segundo intervalo!");
+    }
+    else
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Nenhum!");
+    }
+
+Console.WriteLine("");
+Console.WriteLine("<<<<<<<<<<<<<<<<<<<<<<<<<<(^.^)>>>>>>>>>>>>>>>>>>>>>>>>");
+Console.WriteLine("Fim Algoritimo");    
