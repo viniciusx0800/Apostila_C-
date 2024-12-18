98ae3bb8b6f62440bed5ff2b67710136cf4a1b95
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-18 10:21:29 -0300

    feat : Atividade da Pirâmide de alimento

--- 25PiramideDeAlimentos/Program.cs
+++ 25PiramideDeAlimentos/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Lê o número de níveis da pirâmide"); 
+        int N = int.Parse(Console.ReadLine());
+
+       
+        for (int J = 1; J <= N; J++)
+        {
+            int espacos = N - J;
+
+            int quantidade = J * 2 - 1;
+
+            for (int i = 0; i < espacos; i++)
+            {
+                Console.Write(" "); 
+            }
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                Console.Write(J); 
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
