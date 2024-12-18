ead576eda914f224ec9c67a70758ad13ef376dfc
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-12-18 17:10:45 -0300

    ainda inprogress

--- 10FazendoGol/Program.cs
+++ 10FazendoGol/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string drible, bloqueio, chute, goleiro;
+
+        Console.WriteLine("Insira o lado que o atacante driblou (d para direita, e para esquerda):");
+        drible = Console.ReadLine();
+
+        Console.WriteLine("Insira o lado que o zagueiro bloqueou (d para direita, e para esquerda):");
+        bloqueio = Console.ReadLine();
+
+        Console.WriteLine("Insira o lado que o atacante chutou (d para direita, e para esquerda):");
+        chute = Console.ReadLine();
+
+        Console.WriteLine("Insira o lado que o goleiro pulou (d para direita, e para esquerda):");
+        goleiro = Console.ReadLine();
+ 
+    
+
+            
+        }
+    }
+}
